85424a653be85b9b675d80e6f600ab2ddbf6b0e2
adrianomanicoba <93198936+adrianomanicoba@users.noreply.github.com>
2023-01-11 17:07:27 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# criar a 1ª página do site
+
+# route
+
+# função
+@app.route("/")
+def homepage():
+    return "<p>Esse é meu 1º </p>site teste teste"
+# colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    # servidor do render
